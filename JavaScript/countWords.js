95dc41dfb56703ebcd32c8179052aa8c9af32d68
Anina Angelova <anina_1999@abv.bg>
2025-08-12 12:41:39 +0300

    Create countWords.js

---
+++ JavaScript/countWords.js
@@ -0,0 +1,18 @@
+function countWords(words) {
+    let wordsObject = {};
+
+    for (let i = 0; i < words.length; i++) {
+        let currentIndex = words[i];
+        let count = 1;
+
+        if (!wordsObject[currentIndex]) {
+            wordsObject[currentIndex] = count;
+        } else {
+            wordsObject[currentIndex] += count;
+        }
+    }
+
+    return wordsObject;
+}
+
+//console.log(countWords(["apple", "banana", "apple", "orange", "banana", "apple"]));
