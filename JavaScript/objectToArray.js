13fc56c0e1185fa79daab9918c14ddfa623be053
Anina Angelova <anina_1999@abv.bg>
2025-08-12 12:42:41 +0300

    Create objectToArray.js

---
+++ JavaScript/objectToArray.js
@@ -0,0 +1,11 @@
+//solve #1 (works for current keys)
+function objectToArray(obj) {
+    let array = [];
+    array.push("name: " + obj.name);
+    array.push("age: " + obj.age);
+    array.push("city: " + obj.city);
+
+    return array;
+}
+
+console.log(objectToArray({ name: "John", age: 30, city: "Sofia" }));
